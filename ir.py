6e2e70b7ea6299966691164edc8ef4c7d7297d6e
ahylden <ahylden@yahoo.com>
2023-09-14 23:59:22 -0500

    updating ir py

--- ir.py
+++ ir.py
@@ -6,15 +6,15 @@ sensor = 16
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(sensor,GPIO.IN)
 
-print "IR Sensor Ready....."
-print " "
+print("IR Sensor Ready.....")
+print(" ")
 
 try: 
    while True:
       if GPIO.input(sensor):
-          print "Object Detected"
-          while GPIO.input(sensor):
-              time.sleep(0.2)
+        print("Object Detected")
+        while GPIO.input(sensor):
+            time.sleep(0.2)
 
 
 except KeyboardInterrupt:
