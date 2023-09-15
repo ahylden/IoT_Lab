75c65c8b8837be12d95debd8a3186d50fce2a781
ahylden <ahylden@yahoo.com>
2023-09-15 00:20:46 -0500

    added wait time to ir check

--- ir.py
+++ ir.py
@@ -13,10 +13,9 @@ try:
    while True:
         if GPIO.input(sensor):
             print("Object Detected")
-            while GPIO.input(sensor):
-                time.sleep(0.2)
         else:
             print("No Object Dectected")
+        time.sleep(.5)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
