5b741a2cf11917864a1c9e164917243dc402d104
ahylden <ahylden@yahoo.com>
2023-09-15 00:17:10 -0500

    adding no object found code

--- ir.py
+++ ir.py
@@ -11,11 +11,12 @@ print(" ")
 
 try: 
    while True:
-      if GPIO.input(sensor):
-        print("Object Detected")
-        while GPIO.input(sensor):
+        if GPIO.input(sensor):
+            print("Object Detected")
+            while GPIO.input(sensor):
             time.sleep(0.2)
-
+        else:
+            print("No Object Dectected")
 
 except KeyboardInterrupt:
     GPIO.cleanup()
