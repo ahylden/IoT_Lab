b63ded8835228fbab40e209d3cc0f03be26fad22
ahylden <ahylden@yahoo.com>
2023-10-18 16:37:43 -0500

    removed comments

--- src/aws_connect.py
+++ src/aws_connect.py
@@ -15,17 +15,5 @@ client.tls_insecure_set(True)
 client.connect("a3vqslwjgydv4a-ats.iot.us-east-2.amazonaws.com", 8883, 60)
 
 def publishData(txt):
-    #json update format per aws website
-    #statusUpdate = { "state" : {   
-    #                "reported" :
-    #                        {"detected" : txt
-    #                    }
-    #                }
-    #            }
-
     timeDetected = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
     client.publish("raspi/data", payload=json.dumps({"time": timeDetected, "detectionmethod": txt}), qos=0, retain=False)
-        
-#_thread.start_new_thread(publishData,("Spin-up new Thread...",))
-
-#client.loop_forever()
