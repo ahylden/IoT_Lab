7a4ff62787cce95138c292e943ad51b1e4f52ffe
ahylden <ahylden@yahoo.com>
2023-10-22 17:47:20 -0500

    adding facial recognition

---
+++ src/fr.py
@@ -0,0 +1,38 @@
+import face_recognition
+import os, sys
+import cv2
+import numpy as np
+import math
+
+def face_confidence(face_distance, face_match_threshold=0.6):
+    range = (1.0 - face_match_threshold)
+    linear_val = (1.0 - face_distance) / (range * 2.0)
+
+    if face_distance > face_match_threshold:
+        return str(round(linear_val* 100, 2)) + '%'
+    else:
+        value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - .05) * 2, 0.2))) * 100
+        return str(round(value, 2)) + '%'
+    
+class FaceRecognition:
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    known_face_encodings = []
+    known_face_names = []
+    process_current_frame = True
+
+    def __init__(self):
+        self.encode_faces()
+
+    def encode_faces(self):
+        for image in os.listdir('face'):
+            face_image = face_recognition.load_image_file(f'faces/{image}')
+            face_encoding = face_recognition.face_encodings(face_image)[0]
+
+            self.known_face_encodings.append(face_encoding)
+            self.known_face_names.append(image)
+        print(self.known_face_names)
+
+if __name__ == '__main__':
+    fr = FaceRecognition()
