b8939478e87405f4d1b867f8228cf780146c364b
ahylden <ahylden@yahoo.com>
2023-10-29 21:41:31 -0500

    uncommenting

--- src/fr.py
+++ src/fr.py
@@ -14,9 +14,9 @@ speaker = 36
 
 armed = True
 
-#GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(sensor,GPIO.IN)
-#GPIO.setup(speaker,GPIO.OUT)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(sensor,GPIO.IN)
+GPIO.setup(speaker,GPIO.OUT)
 
 def face_confidence(face_distance, face_match_threshold=0.6):
     range = (1.0 - face_match_threshold)
